4d06d57534a34b0b840c2b56d6c64f4fefc8dddf
mekameka2020 <71114217+mekameka2020@users.noreply.github.com>
2021-01-12 15:46:39 +0900

    Update call.c

--- call.c
+++ call.c
@@ -1,3 +1,10 @@
+/*
+  Copyright (c) 2021 mekameka2020
+
+  This software is released under the MIT License.
+  http://opensource.org/licenses/mit-license.php
+ */
+
 #include <stdio.h>
 
 int main()
