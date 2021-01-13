6440d174fcedfcde34a02d0608c6aa93e7d72477
mekameka2020 <71114217+mekameka2020@users.noreply.github.com>
2021-01-13 14:40:11 +0900

    Update calculate_BMI.c

--- calculate_BMI.c
+++ calculate_BMI.c
@@ -8,30 +8,35 @@
 #include<stdio.h>
 #include<math.h>
 
-int main()
-{
- int h, w, hm, hm2, BMI;
- printf("あなたの身長を入力してください:");
- scanf("%d",&h);
- hm = h / 100;
- printf("あなたの体重を入力してください:");
- scanf("%d",&w);
- hm2 = hm ^ 2;
- BMI = w / hm2;
- printf("あなたのBMIは%dです。\n", BMI);
- 
-  if (BMI < 18.5)
-	  printf("あなたは『低体重』です。\n");
-  else if (18.5 <= BMI < 25)
-	  printf("あなたは『普通体重』です。\n");
-  else if (25 <= BMI < 30)
-	  printf("あなたは『肥満(１度)』です。\n");
-  else if (30 <= BMI < 35)
-	  printf("あなたは『肥満(２度)』です。\n");
-  else if(35 <= BMI < 40)
-	  printf("あなたは『肥満(３度)』です。\n");
-  else if(40 <= BMI)
-	  printf("あなたは『肥満(４度)』です。\n");
+int main(){
+//変数の宣言
+int h, w, hm, hm2, BMI;
+//身長の入力要求
+printf("あなたの身長を入力してください:");
+scanf("%d",&h);
+//身長cmのm換算
+hm = h / 100;
+//体重の入力要求
+printf("あなたの体重を入力してください:");
+scanf("%d",&w);
+//BMI計算
+hm2 = hm ^ 2;
+BMI = w / hm2;
+//BMI表示
+printf("あなたのBMIは%dです。\n", BMI);
+	//BMIレベルの分類
+	if (BMI < 18.5)
+		printf("あなたは『低体重』です。\n");
+  	else if (18.5 <= BMI < 25)
+		printf("あなたは『普通体重』です。\n");
+	else if (25 <= BMI < 30)
+		printf("あなたは『肥満(１度)』です。\n");
+  	else if (30 <= BMI < 35)
+		printf("あなたは『肥満(２度)』です。\n");
+  	else if(35 <= BMI < 40)
+		printf("あなたは『肥満(３度)』です。\n");
+  	else if(40 <= BMI)
+		printf("あなたは『肥満(４度)』です。\n");
 
  return 0;
 }
